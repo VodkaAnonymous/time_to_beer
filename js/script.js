b3586dd9a8093c6d0d04eabff0c465a430d6bcfd
Manea Eugen <e.manea@youwe.nl>
2015-08-22 08:31:36 +0200

    Clean up javascript

--- js/script.js
+++ js/script.js
@@ -9,27 +9,38 @@ function vrun() {
     video.style.display = 'block';
     video.play();
 }
+var fridayCalculator = {
+    jumpToNextFriday: function (date) {
+        return new Date(+date + (7 - (date.getDay() + 2) % 7) * 86400000);
+    },
+
+    calculateNextFriday: function () {
+        var now = new Date();
+        var nextFriday;
+        if (now.getDay() === 5) {
+            nextFriday = new Date(now);
+        } else {
+            nextFriday = this.jumpToNextFriday(now);
+        }
+
+        nextFriday.setHours(17);
+        nextFriday.setMinutes(0);
+        nextFriday.setSeconds(0);
+
+        return (nextFriday.getTime() / 1000) - (now.getTime() / 1000);
+    }
+};
+
 
 $(function () {
     FlipClock.Lang.Custom = {days: 'Days', hours: 'Hours', minutes: 'Minutes', seconds: 'Seconds'};
     var opts =
     {
         clockFace: 'DailyCounter',
-        //clockFace: 'HourCounter',
         countdown: true,
         language: 'Custom'
     };
-    var countdown = (calculateNextFriday() + (((60 * 60) * 24) * 7)) - ((new Date().getTime()) / 1000);
-    countdown = Math.max(3, countdown);
+    var countdown = fridayCalculator.calculateNextFriday();
+    countdown = Math.max(3, countdown); // 3 seconds should be enough to open a beer
     $('.clock-builder-output').FlipClock(countdown, opts);
 });
-jumpToNextFriday = function (date) {
-    return new Date(+date + (7 - (date.getDay() + 2) % 7) * 86400000);
-};
-function calculateNextFriday() {
-    var today = new Date();
-    today.setHours(17);
-    today.setMinutes(0);
-    today.setSeconds(0);
-    return Math.floor(today / 1000);
-}
